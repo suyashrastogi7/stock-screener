cc88669cc1d4dcd20a697d1fa6b73e225dc52f4b
Mihir Harshe <mihir.harshe12@gmail.com>
2021-12-04 12:59:21 +0530

    Changed folder structure

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 var app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
@@ -271,7 +271,7 @@ app.get("/bse/getTopTurnOvers", (req, res, next) => {
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
 
